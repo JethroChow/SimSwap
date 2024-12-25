9fe02d752fdb1e80100062ee5956f1470a1f88c8
JethroChow <tidesnn@gmail.com>
2024-12-25 18:14:34 +0800

    update

--- models/fs_model.py
+++ models/fs_model.py
@@ -42,13 +42,11 @@ class fsModel(BaseModel):
 
         return loss_filter
 
-    def initialize(self, opt):
+    def initialize(self, device, netArc, opt):
         BaseModel.initialize(self, opt)
         if opt.resize_or_crop != 'none' or not opt.isTrain:  # when training at full res this causes OOM
             torch.backends.cudnn.benchmark = True
         self.isTrain = opt.isTrain
-
-        device = torch.device("cuda:0")
 
         if opt.crop_size == 224:
             from .fs_networks import Generator_Adain_Upsample, Discriminator
@@ -59,12 +57,10 @@ class fsModel(BaseModel):
         self.netG = Generator_Adain_Upsample(input_nc=3, output_nc=3, latent_size=512, n_blocks=9, deep=False)
         self.netG.to(device)
 
+        self
+        
         # Id network
-        netArc_checkpoint = opt.Arc_path
-        netArc_checkpoint = torch.load(netArc_checkpoint, map_location=torch.device("cpu"))
-        self.netArc = netArc_checkpoint
-        self.netArc = self.netArc.to(device)
-        self.netArc.eval()
+
 
         if not self.isTrain:
             pretrained_path = '' if not self.isTrain else opt.load_pretrain
